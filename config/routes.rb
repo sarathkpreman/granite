46cd83a7e5f81955d88db8854c26f8100d5fd3fc
Sarath K P <ssarathckm@gmail.com>
2024-09-15 18:06:07 +0530

    Added login feature

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
     resources :tasks, except: %i[new edit], param: :slug
     resources :users, only: :index
     resources :users, only: %i[index create]
+    resource :session, only: :create
   end
 
   root "home#index"
