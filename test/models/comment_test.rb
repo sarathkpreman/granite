13dd61da6970a07415ac18c8d2f398692c94ff07
Sarath K P <ssarathckm@gmail.com>
2024-10-01 09:34:33 +0530

    Changes made in comments_test.rb

--- test/models/comment_test.rb
+++ test/models/comment_test.rb
@@ -7,8 +7,6 @@ class CommentTest < ActiveSupport::TestCase
     @comment = build(:comment)
   end
 
-  # embed new test cases here...
-
   def test_comment_should_be_invalid_without_content
     @comment.content = ""
     assert @comment.invalid?
